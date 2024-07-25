c3dfb287d1894bd54e879e56a96d7205c78b3f5f
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-07-25 19:22:19 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -5,7 +5,7 @@ upper = 100
 
 for num in range(lower, upper + 1):
    # all prime numbers are greater than 1
-   print("DA")
+   print("DAP")
    if num > 1:
        for i in range(2, num):
            if (num % i) == 0:
