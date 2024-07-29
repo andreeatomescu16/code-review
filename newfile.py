ead5f69c3279e733f75849c1921b44782b6612d3
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-07-29 09:27:13 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -5,7 +5,7 @@ upper = 99
 
 for num in range(lower, upper + 1):
    print("DOAMNE AJUTA")
-       for i in range(3, num):
+       for i in range(2, num):
            if (num % i) == 0:
                break
        else:
