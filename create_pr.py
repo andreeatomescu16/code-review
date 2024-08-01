64201d78f618ce73c4eed28994dc20d88b9c19f5
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-08-01 10:45:18 +0300

    Update create_pr.py

--- create_pr.py
+++ create_pr.py
@@ -8,7 +8,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Load environment variables from a .env file if present
 load_dotenv()
 
 # Get environment variables
