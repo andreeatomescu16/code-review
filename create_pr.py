24151c4f0ef93308489f8c029c3c9b15cc2f55c8
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-08-02 11:19:58 +0300

    Update create_pr.py

--- create_pr.py
+++ create_pr.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 # Get environment variables
 GITHUB_TOKEN = os.getenv("MY_GITHUB_TOKEN")
-REPO_NAME = "andreea-ghe/test"
+REPO_NAME = "andreeatomescu16/code-review"
 
 if not GITHUB_TOKEN or not REPO_NAME:
     raise ValueError("Missing GITHUB_TOKEN or GITHUB_REPOSITORY environment variable")
