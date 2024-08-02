a7efd896c45afa2a092a6031a7517eb9096aa36d
andreeatomescu16 <andreeatomescu040@gmail.com>
2024-08-02 11:08:35 +0300

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -4,7 +4,7 @@ lower = 10
 upper = 30
 
 for num in range(lower, upper + 1):
-       for i in range(4, num):
+       for i in range(2, num):
            if (num % i) == 0:
                break
        else:
